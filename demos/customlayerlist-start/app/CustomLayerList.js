2c193ac5a694584fba1dd841d0543804f3a8fba1
Matt Driscoll <mdriscoll@esri.com>
2017-06-28 16:29:21 -0700

    some progress

--- demos/customlayerlist-start/app/CustomLayerList.js
+++ demos/customlayerlist-start/app/CustomLayerList.js
@@ -1,7 +1,37 @@
-(function( $ ) {
- 
-    $.fn.customLayerList = function( action ) {
-      alert("hi");
-    };
- 
-}( jQuery ));
+(function ($) {
+
+  $.fn.customLayerList = function (options) {
+    // set some defaults
+    var defaults = {};
+    // merge
+    var options = $.extend({}, defaults, options);
+
+    var vm = options.viewModel;
+    var obj = $(this);
+
+    var operationalItems = vm.operationalItems;
+
+    console.log(operationalItems.length);
+
+    var list = $("<ul>");
+
+    vm.operationalItems.on("change", function (event) {
+      console.log("changed", event);
+
+      list.empty();
+
+
+      operationalItems.forEach(function (item) {
+
+        var itemNode = $("<li>");
+        itemNode.text(item.title);
+
+        list.append(itemNode);
+      }, this);
+
+    });
+
+    return this.append(list);
+  };
+
+}(jQuery));
