b1b10d93f9564ff46028c1fdbd52d949855104cd
Matt Driscoll <MDriscoll@esri.com>
2017-06-29 16:28:35 -0700

    progress on demo

--- demos/customlayerlist-start/app/CustomLayerList.js
+++ demos/customlayerlist-start/app/CustomLayerList.js
@@ -11,29 +11,80 @@
 
     var operationalItems = vm.operationalItems;
 
-    console.log(operationalItems.length);
+    var groupNode = $('<div class="list-group">');
 
-    var list = $("<ul>");
-    list.addClass("list-group");
+    vm.operationalItems.on("change", function () {
+      groupNode.empty();
 
-    vm.operationalItems.on("change", function (event) {
-      console.log("changed", event);
+      function setUpdatingClass(item, updatingNode) {
+        item.updating ?
+          updatingNode.removeClass("hidden") :
+          updatingNode.addClass("hidden");
+      }
 
-      list.empty();
+      function setActiveClass(item, itemNode) {
+        item.visible ? itemNode.addClass("active") : itemNode.removeClass("active");
+      }
 
+      function setDisabledClass(item, itemNode) {
+        item.visibleAtCurrentScale ? itemNode.removeClass("disabled") : itemNode.addClass("disabled");
+      }
+
+      function addChildren(childGroupNode, children) {
+        childGroupNode.empty();
+
+        children.length ? childGroupNode.removeClass("hidden") :
+          childGroupNode.addClass("hidden");
+
+        children.forEach(function (child) {
+          createItemNode(child, childGroupNode)
+        });
+      }
+
+      function createItemNode(item, parentNode) {
+        var itemNode = $('<button type="button" class="list-group-item" />');
+        itemNode.text(item.title);
+        var updatingNode = $('<span class="badge"><span class="glyphicon glyphicon-refresh" aria-hidden="true" /></span>');
+        itemNode.append(updatingNode);
+        var childGroupNode = $('<div class="list-group hidden" />');
+        itemNode.append(childGroupNode);
+
+        setActiveClass(item, itemNode);
+        item.watch("visible", function () {
+          setActiveClass(item, itemNode);
+        });
+
+        setUpdatingClass(item, updatingNode);
+        item.watch("updating", function () {
+          setUpdatingClass(item, updatingNode);
+        });
+
+        setDisabledClass(item, itemNode);
+        item.watch("visibleAtCurrentScale", function () {
+          setDisabledClass(item, itemNode);
+        });
+
+        addChildren(childGroupNode, item.children);
+        item.children.on("change", function () {
+          addChildren(childGroupNode, item.children);
+        });
+
+        itemNode.on("click", function (event) {
+          item.visible = !item.visible;
+          event.stopPropagation();
+        });
+
+        parentNode.append(itemNode);
+      }
 
       operationalItems.forEach(function (item) {
-
-        var itemNode = $("<li>");
-        itemNode.text(item.title);
-        itemNode.addClass("list-group-item");
-
-        list.append(itemNode);
-      }, this);
+        createItemNode(item, groupNode);
+      });
 
     });
 
-    return this.append(list);
+    return this.append(groupNode);
   };
 
 }(jQuery));
+
